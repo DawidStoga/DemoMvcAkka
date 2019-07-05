539e585e59a5620b9c863bba7c7267b228935f9d
Dawid Stoga PGS <dstoga@pgs-soft.com>
2019-07-05 09:23:02 +0200

    comments added

--- WebApplication3/Akka/Actors/PersonManagerActor.cs
+++ WebApplication3/Akka/Actors/PersonManagerActor.cs
@@ -12,6 +12,38 @@ using ApiDemo.Models;
 
 namespace ApiDemo.Akka
 {
+    #region wrong-solution 
+    //public PersonManagerActor(CompanyContext dbContext)
+    //Do not inject DbContext in ctrol ! - because of lifescope of actors
+    #endregion
+
+    #region not-elegant-solution 
+
+    //using (var companyContext = new companyContext(/*connection string*/))
+    //{
+    //    /*...*/
+    //}
+    #endregion
+
+
+    #region better-solution
+    //     inject IServiceScopeFactory in ctor
+    //ManagerActor(IServiceScopeFactory serviceScopeFactory)
+
+    //using (var serviceScope = serviceScopeFactory.CreateScope())
+    //        {
+    //            var dbContext = serviceScope.ServiceProvider.GetService<CompanyContext>();
+    //          .....
+    //       }
+    #endregion
+
+    #region using-akka-extension
+
+    //IExtension - create ActorSystem Extension  - i.e IServiceScope CreateScope()
+    //IExtensionId - Creating, Register, retriving extension with Actorsystem
+    #endregion
+
+
     public class PersonManagerActor: ReceiveActor
     {
         private readonly Dictionary<decimal, Person> _Persons = new Dictionary<decimal, Person>();
@@ -81,9 +113,6 @@ namespace ApiDemo.Akka
 #endif
             });
         }
-
-
-
 
     }
 }
