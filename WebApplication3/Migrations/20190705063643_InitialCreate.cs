3c7964c8949288e84b5884ce00b6901ac1541395
Dawid Stoga PGS <dstoga@pgs-soft.com>
2019-07-05 08:39:39 +0200

    Migration added

---
+++ WebApplication3/Migrations/20190705063643_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiCoreAkkaDemo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "(newid())"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(maxLength: 50, nullable: false),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    AddressLine1 = table.Column<string>(maxLength: 50, nullable: false),
+                    AddressLine2 = table.Column<string>(maxLength: 50, nullable: false),
+                    PostCode = table.Column<string>(maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Pesel = table.Column<decimal>(type: "numeric(11, 0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
